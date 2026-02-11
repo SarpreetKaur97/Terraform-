7554cd4a55a437bbd8c0e441fd0f38f3e429cbec
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-11 16:16:01 +0530

    Update main.tf

--- VPC_Terra/modules/subnets/main.tf
+++ VPC_Terra/modules/subnets/main.tf
@@ -3,6 +3,6 @@ vpc_id = var. aws_vpc.main.id
 cidr_block = var.private_subnet_cidr_block
 availability_zone = var.availability_zone 
 tag = {
- Name = "private_subnet"}
+ Name = "private_subnet_id"}
 }
 
