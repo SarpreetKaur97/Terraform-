617bb68bbbd94a801cb697de64bf6cadfcbb33cc
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-11 15:57:55 +0530

    Add private subnet resource to main.tf

--- VPC_Terra/modules/subnets/main.tf
+++ VPC_Terra/modules/subnets/main.tf
@@ -1,1 +1,8 @@
-private subnet
+resource "aws_subnet" "private_subnet" {
+vpc_id = var. aws_vpc.main.id
+cidr_block = var.private_subnet_cidr_block
+availability_zone = var.availability_zone 
+tag = {
+ Name = "private_subnet"}
+}
+
