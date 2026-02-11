9e553187d29703e23e5cbfb911719158cacdab17
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-11 16:10:05 +0530

    Add variables for private subnet configuration

--- VPC_Terra/modules/subnets/variable.tf
+++ VPC_Terra/modules/subnets/variable.tf
@@ -1,1 +1,13 @@
-variable
+variable "vpc_id" {
+  type = string
+  default = "vpc_de"
+}
+variable "private_subnet_cidr_block" {
+  type = string 
+  default = "10.0.0.0/16"
+}
+variable "availability_zone" {
+  type = string
+  default = "availability_private_zone"
+}
+
