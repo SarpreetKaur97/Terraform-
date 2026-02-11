a5b2f35f57fa256ab25e5cbd69700acf24cc4cb8
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-11 16:27:35 +0530

    Add public subnet resource to main.tf

--- VPC_Terra/modules/subnets/main.tf
+++ VPC_Terra/modules/subnets/main.tf
@@ -5,4 +5,11 @@ availability_zone = var.availability_zone
 tag = {
  Name = "private_subnet_id"}
 }
-
+resource "aws_subnet" "public_subnet" {
+vpc_id = var. aws_vpc.main.id
+cidr_block = var.public_subnet_cidr_block
+availability_zone = var.availability_zone
+map_public_ip_on_launch = true
+tag = {
+ Name = "public_subnet_id"}
+}
