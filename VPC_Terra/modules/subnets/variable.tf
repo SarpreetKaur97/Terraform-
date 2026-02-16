5259cd66925d77b748824cbf59d388879908c489
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 07:00:51 +0530

    Rename vpc_id to aws_vpc and update CIDR blocks
    Updated VPC variable names and CIDR blocks for private and public subnets.

--- VPC_Terra/modules/subnets/variable.tf
+++ VPC_Terra/modules/subnets/variable.tf
@@ -1,23 +1,18 @@
-variable "vpc_id" {
+variable "aws_vpc" {
   type = string
-  default = "vpc_de"
-}
+  }
 variable "private_subnet_cidr_block" {
   type = string 
-  default = "10.0.0.0/16"
+  default = "10.1.0.0/16"
 }
 variable "availability_zone" {
   type = string
   default = "availability_private_zone"
 }
 
-variable "vpc_id" {
-  type = string
-  default = "vpc_de"
-}
 variable "public_subnet_cidr_block" {
   type = string 
-  default = "10.0.0.0/16"
+  default = "10.2.0.0/16"
 }
 variable "availability_zone" {
   type = string
