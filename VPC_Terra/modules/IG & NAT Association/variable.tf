d5eb0890ecc103d009ba66ae4d7a071aeb2fdf9d
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 08:48:54 +0530

    Add variables for VPC NAT and IG association

---
+++ VPC_Terra/modules/IG & NAT Association/variable.tf
@@ -0,0 +1,21 @@
+variable "aws_vpc" {
+  type = string
+}
+variable "eip_id" {
+  type = string
+}  
+variable "public_subnet_id" {
+  type = string
+}
+variable "internet_gateway_id" {
+  type = string
+}
+variable "nat_gateway_id" {
+  type = string
+}
+variable "private_association_id" {
+  type = string
+}
+variable "public_association_id" {
+  type = string
+}
