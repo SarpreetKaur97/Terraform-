869ad5f8ea0ed4589bb1c2dd03e8a91e6bec2430
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 07:13:16 +0530

    Add subnet name variables for private and public subnets

--- VPC_Terra/modules/subnets/variable.tf
+++ VPC_Terra/modules/subnets/variable.tf
@@ -5,6 +5,9 @@ variable "private_subnet_cidr_block" {
   type = string 
   default = "10.1.0.0/16"
 }
+variable "private_subnet_name" {
+  type = string
+}
 variable "availability_zone" {
   type = string
   }
@@ -12,4 +15,6 @@ variable "public_subnet_cidr_block" {
   type = string 
   default = "10.2.0.0/16"
 }
-
+variable "public_subnet_name" {
+  type = string
+}
