a3e87de4009788bbc3c4db9f996f044541c01ea7
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 06:47:52 +0530

    Fix VPC ID reference and update subnet names

--- VPC_Terra/modules/subnets/main.tf
+++ VPC_Terra/modules/subnets/main.tf
@@ -1,15 +1,15 @@
 resource "aws_subnet" "private_subnet" {
-vpc_id = var. aws_vpc.main.id
+vpc_id = var.aws_vpc
 cidr_block = var.private_subnet_cidr_block
 availability_zone = var.availability_zone 
 tag = {
- Name = "private_subnet_id"}
+ Name = "private_subnet"}
 }
 resource "aws_subnet" "public_subnet" {
-vpc_id = var. aws_vpc.main.id
+vpc_id = var.aws_vpc
 cidr_block = var.public_subnet_cidr_block
 availability_zone = var.availability_zone
 map_public_ip_on_launch = true
 tag = {
- Name = "public_subnet_id"}
+ Name = "public_subnet"}
 }
