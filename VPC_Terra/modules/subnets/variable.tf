307540a205f05eb092b4cc18c9a097cffa210dcd
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 07:07:57 +0530

    Refactor availability_zone variable in variable.tf
    Removed duplicate availability_zone variable and adjusted its default value.

--- VPC_Terra/modules/subnets/variable.tf
+++ VPC_Terra/modules/subnets/variable.tf
@@ -7,14 +7,9 @@ variable "private_subnet_cidr_block" {
 }
 variable "availability_zone" {
   type = string
-  default = "availability_private_zone"
-}
-
+  }
 variable "public_subnet_cidr_block" {
   type = string 
   default = "10.2.0.0/16"
 }
-variable "availability_zone" {
-  type = string
-  default = "availability_public_zone"
-}
+
