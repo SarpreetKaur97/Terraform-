409304bafaadadb4e6369444066053966fa70d7f
SarpreetKaur97 <ksarpreet5@gmail.com>
2026-02-16 08:57:59 +0530

    Add AWS security group for web access
    Define a security group with ingress and egress rules for HTTP and HTTPS traffic.

--- VPC_Terra/modules/SG/main.tf
+++ VPC_Terra/modules/SG/main.tf
@@ -1,1 +1,23 @@
-security group
+resource "aws_security_group" "web_sg" {
+name = "sg"
+vpc_id = var.aws_vpc 
+ingress {
+  from_port = 80
+  to_port = 80
+  protocal = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+   }
+ingress {
+  from_port = 443
+  to_port = 443
+  protocal = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+   }
+egress {
+  from_port = 0
+  to_port = 0
+  protocal = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+   }
+
+
